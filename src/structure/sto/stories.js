d3db85270811de7e6a9c3c26c58b452bca7e891a
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-11-16 22:22:38 +0100

    working on carousel function

--- src/structure/sto/stories.js
+++ src/structure/sto/stories.js
@@ -102,92 +102,93 @@ function createStories(){
 
 export{createStories}
 
+// //this is the logic for the carousel
+// function carouselStory(){
+// let slideIndex = 1;
+// let myTimer;
+// let slideshowContainer;
+// let a1 = document.querySelector('.prev-stories');
+// let a2 = document.querySelector('.next-stories');
+// a1.addEventListener('click', go);
+// a2.addEventListener('click', go);
 
-//this is the logic for the carousel
-let slideIndex = 1;
-let myTimer;
-let slideshowContainer;
-let a1 = document.querySelector('.prev');
-let a2 = document.querySelector('.next');
-a1.addEventListener('click', go);
-a2.addEventListener('click', go);
-
-function go(e) {
-  arrow = e.path[0];
-  if (arrow == a2) {
-      plusSlides(1);
-  } else if (arrow == a1) {
-      plusSlides(-1);
-  }
-}
+// function go(e) {
+//   let arrow = e.path[0];
+//   if (arrow == a2) {
+//       plusSlides(1);
+//   } else if (arrow == a1) {
+//       plusSlides(-1);
+//   }
+// }
 
 
-//this is the part of code that creates the carousel
-window.addEventListener("load", function() {
-  showSlides(slideIndex);
-  myTimer = setInterval(function() {
-      plusSlides(1)
-  }, 4000);
+// //this is the part of code that creates the carousel
+// window.addEventListener("load", function() {
+//   showSlides(slideIndex);
+//   myTimer = setInterval(function() {
+//       plusSlides(1)
+//   }, 4000);
 
-  slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
+//   slideshowContainer = document.getElementsByClassName('slideshow-inner-stories')[0];
 
-  slideshowContainer.addEventListener('mouseenter', pause)
-  slideshowContainer.addEventListener('mouseleave', resume)
-})
+//   slideshowContainer.addEventListener('mouseenter', pause)
+//   slideshowContainer.addEventListener('mouseleave', resume)
+// })
 
-//Controls the current slide and resets interval if needed
-function currentSlide(n) {
-  clearInterval(myTimer);
-  myTimer = setInterval(function() {
-      plusSlides(n + 1)
-  }, 4000);
-  showSlides(slideIndex = n);
-}
+// //Controls the current slide and resets interval if needed
+// function currentSlide(n) {
+//   clearInterval(myTimer);
+//   myTimer = setInterval(function() {
+//       plusSlides(n + 1)
+//   }, 4000);
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  let slides = document.getElementsByClassName("mySlides");
-  let i;
-  if (n > slides.length) {
-      slideIndex = 1
-  }
-  if (n < 1) {
-      slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
+// function showSlides(n) {
+//   let slides = document.getElementsByClassName("mySlides-stories");
+//   let i;
+//   if (n > slides.length) {
+//       slideIndex = 1
+//   }
+//   if (n < 1) {
+//       slideIndex = slides.length
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+// }
 
-// NEXT AND PREVIOUS CONTROL
-function plusSlides(n) {
-  clearInterval(myTimer);
-  if (n < 0) {
-      showSlides(slideIndex -= 1);
-  } else {
-      showSlides(slideIndex += 1);
-  }
+// // NEXT AND PREVIOUS CONTROL
+// function plusSlides(n) {
+//   clearInterval(myTimer);
+//   if (n < 0) {
+//       showSlides(slideIndex -= 1);
+//   } else {
+//       showSlides(slideIndex += 1);
+//   }
 
-  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+//   //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
 
-  if (n === -1) {
-      myTimer = setInterval(function() {
-          plusSlides(n + 2)
-      }, 4000);
-  } else {
-      myTimer = setInterval(function() {
-          plusSlides(n + 1)
-      }, 4000);
-  }
-}
+//   if (n === -1) {
+//       myTimer = setInterval(function() {
+//           plusSlides(n + 2)
+//       }, 4000);
+//   } else {
+//       myTimer = setInterval(function() {
+//           plusSlides(n + 1)
+//       }, 4000);
+//   }
+// }
 
-pause = () => {
-  clearInterval(myTimer);
-}
+// let pause = () => {
+//   clearInterval(myTimer);
+// }
 
-resume = () => {
-  clearInterval(myTimer);
-  myTimer = setInterval(function() {
-      plusSlides(slideIndex)
-  }, 4000);
-}
+// let resume = () => {
+//   clearInterval(myTimer);
+//   myTimer = setInterval(function() {
+//       plusSlides(slideIndex)
+//   }, 4000);
+// }
+// }
